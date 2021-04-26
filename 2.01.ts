8a1150621515b903f724b47efc59c63dd4c1ba07
narojishubham <shubham.naroji@gmail.com>
2021-04-26 14:54:39 +0530

    prime number

--- 2.01.ts
+++ 2.01.ts
@@ -1,17 +1,18 @@
-export function isPrime(num: number) {
+function isPrime <T extends number> (num:T) {
     if (num === 2) {
-      return true;
-    } else if (num > 1) {
-      for (let i = 2; i < num; i++) {
-        if (num % i !== 0) {
-          return true;
-        } else if (num === i * i) {
-          return false;
-        } else {
-          return false;
+        return true;
+      } else if (num > 1) {
+        for (let i = 2; i < num; i++) {
+          if (num % i !== 0) {
+            return true;
+          } else if (num === i * i) {
+            return false;
+          } else {
+            return false;
+          }
         }
+      } else {
+        return false;
       }
-    } else {
-      return false;
-    }
-  }
+}
+console.log(isPrime(4))
