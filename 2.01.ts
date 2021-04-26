115660b270a0950f3c2e11e8d6191a88f810c2a6
narojishubham <shubham.naroji@gmail.com>
2021-04-26 14:36:29 +0530

    prime number

---
+++ 2.01.ts
@@ -0,0 +1,17 @@
+export function isPrime(num: number) {
+    if (num === 2) {
+      return true;
+    } else if (num > 1) {
+      for (let i = 2; i < num; i++) {
+        if (num % i !== 0) {
+          return true;
+        } else if (num === i * i) {
+          return false;
+        } else {
+          return false;
+        }
+      }
+    } else {
+      return false;
+    }
+  }
